fe0012960e965a55c587d6c4b2485c07b85cc3f9
martines3000 <domajnko.martin@gmail.com>
2023-02-20 19:01:56 +0100

    fix: should fix now-playing api

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -1,27 +1,17 @@
-import { type NextRequest } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { getNowPlaying } from '../../lib/spotify';
 
-export default async function handler(_: NextRequest) {
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   const response = await getNowPlaying();
 
   if (response.status === 204 || response.status > 400) {
-    return new Response(JSON.stringify({ isPlaying: false }), {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-      },
-    });
+    return res.status(200).json({ isPlaying: false });
   }
 
   const song = await response.json();
 
   if (song.item === null) {
-    return new Response(JSON.stringify({ isPlaying: false }), {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-      },
-    });
+    return res.status(200).json({ isPlaying: false });
   }
 
   const isPlaying = song.is_playing;
@@ -33,21 +23,17 @@ export default async function handler(_: NextRequest) {
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
 
-  return new Response(
-    JSON.stringify({
-      album,
-      albumImageUrl,
-      artist,
-      isPlaying,
-      songUrl,
-      title,
-    }),
-    {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-        'cache-control': 'public, s-maxage=60, stale-while-revalidate=30',
-      },
-    }
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=60, stale-while-revalidate=30'
   );
+
+  return res.status(200).json({
+    album,
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title,
+  });
 }
