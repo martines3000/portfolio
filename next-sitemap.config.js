daa7e2dbe09b26dead96df979c4110d6e54c33a7
martines3000 <domajnko.martin@gmail.com>
2023-03-02 13:06:10 +0100

    feat: add next-sitemap support

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://portfolio.skippy-ai.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 1000,
+};
+
+module.exports = config;
